26c8689ceb6895e55823171836b993c18368a431
Arnaud de Bossoreille <arnaud.debossoreille@gmail.com>
2026-02-11 22:08:08 +0100

    Windows named pipe

---
+++ src/attach/windows_named_pipe.rs
@@ -0,0 +1,174 @@
+//! Attach API using a named pipe
+
+use std::time::Duration;
+
+use async_io::{
+    os::windows::{NamedPipeListener, NamedPipeStream},
+    Timer,
+};
+use async_stream::try_stream;
+use futures::Stream;
+
+use crate::attach::{Attacher, AttacherSignal};
+
+/// Starts listening for attach signals and return incoming connections as a async `Stream`.
+///
+/// In order to stop accepting connections, it is enough to stop polling the stream.
+pub fn listen<A>() -> impl Stream<Item = Result<(NamedPipeStream, ()), Box<dyn std::error::Error>>>
+where
+    A: Attacher,
+{
+    // It is important to keep this in the synchronous part in order to ensure the listening
+    // process is ready to accept attachment requests even if the future is not awaited.
+    //
+    // Nevertheless, the error will only be raised if the future is awaited.
+    let signaled = A::signaled();
+
+    try_stream! {
+
+        signaled.await?;
+
+        let listener = NamedPipeListener::bind(named_pipe_name(std::process::id()))?;
+
+        loop {
+            let stream = listener.accept().await?;
+            yield (stream, ());
+        }
+    }
+}
+
+/// Connects to a process identified by its ID.
+///
+/// Returns the opened pipe on success.
+pub async fn connect<A>(pid: u32) -> Result<NamedPipeStream, Box<dyn std::error::Error>>
+where
+    A: Attacher,
+{
+    let name = named_pipe_name(pid);
+
+    let mut signal = A::signal(pid)?;
+
+    let mut attempts = 0;
+
+    while attempts < 100 {
+        match NamedPipeStream::connect(&name).await {
+            Ok(stream) => {
+                return Ok(stream);
+            }
+            Err(err) => {
+                dbg!(err);
+            }
+        }
+
+        signal.send().await?;
+
+        Timer::after(Duration::from_millis(100)).await;
+
+        attempts += 1;
+    }
+    return Err(format!(
+        "Unable to open named pipe {}: target process {} doesn't respond",
+        name, pid
+    )
+    .into());
+}
+
+fn named_pipe_name(pid: u32) -> String {
+    format!(r"\\.\pipe\.teleop_pid_{pid}")
+}
+
+#[cfg(test)]
+#[cfg_attr(coverage_nightly, coverage(off))]
+mod tests {
+    use std::pin::pin;
+
+    use futures::{
+        channel::oneshot,
+        io::{BufReader, BufWriter},
+        AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, StreamExt,
+    };
+
+    use super::*;
+    use crate::attach::{tests::ATTACHER_TEST_MUTEX, DefaultAttacher};
+
+    #[test]
+    fn test_named_pipe_attachment() {
+        // This test may conflict with attacher tests
+        let _attacher_test = ATTACHER_TEST_MUTEX.lock();
+
+        let (sender, receiver) = oneshot::channel::<()>();
+
+        let server = || -> Result<(), Box<dyn std::error::Error>> {
+            let mut exec = futures::executor::LocalPool::new();
+
+            let res = exec.run_until(async {
+                let mut conn_stream = pin!(listen::<DefaultAttacher>());
+                println!("server is listening");
+                sender.send(()).unwrap();
+                if let Some(stream) = conn_stream.next().await {
+                    println!("server received connection");
+                    let (stream, _addr) = stream?;
+                    let (input, output) = stream.split();
+                    let mut input = BufReader::new(input);
+                    let mut output = BufWriter::new(output);
+
+                    let mut read = String::new();
+                    while input.read_line(&mut read).await? == 0 {}
+                    assert_eq!(read, "ping\n");
+                    println!("server received ping");
+
+                    output.write_all("pong\n".as_bytes()).await?;
+                    output.flush().await?;
+                    println!("server wrote pong");
+                }
+
+                Ok::<_, Box<dyn std::error::Error>>(())
+            });
+
+            exec.run();
+
+            res?;
+
+            Ok(())
+        };
+
+        let client = || -> Result<(), Box<dyn std::error::Error>> {
+            let pid = std::process::id();
+
+            let mut exec = futures::executor::LocalPool::new();
+
+            let res = exec.run_until(async move {
+                let () = receiver.await?;
+                println!("client is initiating connection");
+                let stream = connect::<DefaultAttacher>(pid).await?;
+                let (input, output) = stream.split();
+                let mut input = BufReader::new(input);
+                let mut output = BufWriter::new(output);
+                println!("client is connected");
+                output.write_all("ping\n".as_bytes()).await?;
+                output.flush().await?;
+                println!("client wrote ping");
+
+                let mut read = String::new();
+                while input.read_line(&mut read).await? == 0 {}
+                assert_eq!(read, "pong\n");
+                println!("client received pong");
+
+                Ok::<_, Box<dyn std::error::Error>>(())
+            });
+
+            exec.run();
+
+            res?;
+
+            Ok(())
+        };
+
+        let s = std::thread::spawn(|| server().unwrap());
+        // Improve code coverage by letting the server avoid early returns
+        std::thread::sleep(Duration::from_secs(2));
+        let c = std::thread::spawn(|| client().unwrap());
+        c.join().unwrap();
+        s.join().unwrap();
+    }
+}
